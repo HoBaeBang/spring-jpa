838af7aad5c91af49e801a9c1712f928cac938d1
hobae <bhb0407@naver.com>
2022-03-10 23:53:32 +0900

    jpa 1차 캐시

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,4 +1,6 @@
 package hellojpa;
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -13,25 +15,38 @@ public class JpaMain {
 
         EntityTransaction tx = em.getTransaction();
         tx.begin();
+
         try {
 //            Member member = new Member();
-//            member.setId(2L);
-//            member.setName("helloB");
+//            member.setId(101L);
+//            member.setName("helloJPA");
 //            Member findMember = em.find(Member.class, 1L);
 //            System.out.println("findMember.id = " + findMember.getId());
 //            System.out.println("findMember.name = " + findMember.getName());
 //            em.remove(findMember);
 //            findMember.setName("helloJpaz");
 
-            List<Member> result = em.createQuery("select m from  Member as m", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(8)
-                    .getResultList();
+//            List<Member> result = em.createQuery("select m from  Member as m", Member.class)
+//                    .setFirstResult(1)
+//                    .setMaxResults(8)
+//                    .getResultList();
+//
+//            for (Member member : result) {
+//                System.out.println("member.getName() = " + member.getName());
+//            }
 
-            for (Member member : result) {
-                System.out.println("member.getName() = " + member.getName());
-            }
-            
+
+            System.out.println("=== BEFORE ===");
+//            em.persist(member);
+//            em.detach(member);
+            System.out.println("=== BEFORE ===");
+
+            Member member1 = em.find(Member.class, 101L);
+            Member member2 = em.find(Member.class, 101L);
+
+//            System.out.println("member.getId() = " + member.getId());
+//            System.out.println("member.getName() = " + member.getName());
+
             tx.commit();
 //            em.persist(member);
         }
